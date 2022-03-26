ff74216d35632de6f6a814ba758d39453dfb235e
marcon29 <online@markstabler.com>
2022-03-25 19:12:29 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+ var customerName = "bob"
+ const leastFavoriteCustomer = "jim bob"
+
+ function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+ }
+
+function setBestCustomer() {
+    bestCustomer = "not bob"
+}
+
+function overwriteBestCustomer(customer) {
+    bestCustomer = customer
+}
+
+function changeLeastFavoriteCustomer(customer) {
+    leastFavoriteCustomer = "bobby jimmy"
+}
+
